d995f4fc13bd6b05522c863dcc3ce709a849337b
richjdsmith <rich@richjdsmith.ca>
2017-04-12 13:11:21 -0700

    Completed Create and Read of CRUD

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -1,7 +1,40 @@
 defmodule Discuss.TopicController do
   use Discuss.Web,  :controller
 
-  def new(conn, params) do
+  alias Discuss.Topic
 
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+
+    render conn, "index.html", topics: topics
+  end
+
+  def new(conn, _params) do
+    changeset = Topic.changeset(%Topic{}, %{})
+
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} ->
+        conn
+          |> put_flash(:info, "Topic Created!")
+          |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} ->
+        conn
+          |> put_flash(:error, "Your topic wasn't created. Try again.")
+          |>render "new.html", changeset: changeset
+        #render conn, "new.html", changeset: changeset
+    end
+  end
+
+  def edit(conn, %{"id" => topic_id}) do
+     topic = Repo.get(Topic, topic_id)
+     changeset = Topic.changeset(topic)
+
+     render conn, "edit.html", changeset: changeset, topic: topic
   end
 end
